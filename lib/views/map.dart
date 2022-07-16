60699a64194122329784201ee667674c1e2339e2
Asadbek <asadbekabdumajidov7@gmail.com>
2022-07-16 14:10:44 +0500

    map views are set to file

--- lib/views/map.dart
+++ lib/views/map.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapViews extends StatelessWidget {
+class MapViews extends StatefulWidget {
   const MapViews({Key? key}) : super(key: key);
 
   @override
+  State<MapViews> createState() => _MapViewsState();
+}
+
+class _MapViewsState extends State<MapViews> {
+  static const _initialCameraPosition =
+      CameraPosition(target: LatLng(37.773972, -122431297), zoom: 11.5);
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     
+    return const Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: _initialCameraPosition),
     );
   }
 }
